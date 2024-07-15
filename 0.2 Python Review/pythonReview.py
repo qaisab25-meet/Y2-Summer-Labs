ef01ce47227b39a873e935da5b33ec3a3d4384ed
qaisab25-meet <qaisaburaya@gmail.com>
2024-07-16 00:33:25 +0300

    whole code

---
+++ 0.2 Python Review/pythonReview.py
@@ -0,0 +1,39 @@
+def create_youtube_video(title, description):
+    videosyt = {
+        "title": title,
+        "description": description,
+        "likes": 0,
+        "dislikes": 0,
+        "comments": {}
+    }
+    return videosyt
+
+def like(youtubevideo):
+    if "likes" in youtubevideo:
+        youtubevideo["likes"] += 1
+    return youtubevideo
+
+def dislike(youtubevideo):
+    if "dislikes" in youtubevideo:
+        youtubevideo["dislikes"] += 1
+    return youtubevideo
+
+def add_comment(youtubevideo, username, comment_text):
+    youtubevideo["comments"][username] = comment_text
+    return youtubevideo
+
+newyoutubevid = create_youtube_video("Python Tutorial", "Learn Python basics")
+print(newyoutubevid)
+
+newyoutubevid = like(newyoutubevid)
+newyoutubevid = like(newyoutubevid)
+newyoutubevid = dislike(newyoutubevid)
+print(newyoutubevid)
+
+newyoutubevid = add_comment(newyoutubevid, "user", "Great")
+newyoutubevid = add_comment(newyoutubevid, "user3", "good")
+print(newyoutubevid)
+
+for _ in range(495):
+    newyoutubevid = like(newyoutubevid)
+print(newyoutubevid)
